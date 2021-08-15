adcbaf580de4263f79c98ecf395a42eb20c38428
Bharath Kumar <pasupunuri.bharath@gmail.com>
2021-08-15 17:46:12 +0530

    day 1 build

--- App.js
+++ App.js
@@ -2,12 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "./screens/LoginScreen"
+const Stack = createStackNavigator();
+
 export default function App() {
+
+const globleScreenOptions ={
+
+   headerStyle : {backgroundColor : "#2C6BED"},
+   headerTitleStyle :{color : "white"},
+   headerTinColor :"white"
+
+
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator screenOptions ={globleScreenOptions}>
+    <Stack.Screen options={{title : "'lets SignUp"}} name='Login' component={LoginScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firbase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBbSKutNLA8lO0kPOBsDw__LkesUE7cm3g",
+  authDomain: "signal-clone-29e5f.firebaseapp.com",
+  projectId: "signal-clone-29e5f",
+  storageBucket: "signal-clone-29e5f.appspot.com",
+  messagingSenderId: "285996307041",
+  appId: "1:285996307041:web:4c218d80b676ac17dc5020"
+};
